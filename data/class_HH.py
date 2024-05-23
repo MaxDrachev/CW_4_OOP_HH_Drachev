91b7da791b7d4a29b7806966cdf7ce9f6e588178
MaxDrachev <Maksimusdrachev1983@gmail.com>
2024-05-23 03:05:02 +0300

    initiation

---
+++ data/class_HH.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from abc import ABC
+
+
+class Parser(ABC):
+
+    def load_vacancies(self, keyword):
+        pass
+
+
+class HH(Parser):
+    """
+    Класс для работы с API HeadHunter
+    Класс Parser является родительским классом, который вам необходимо реализовать
+    """
+
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'area': self.region, 'page': 0, 'per_page': 100}
+        self.vacancies = []
+
+    def load_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+
+        return self.vacancies
